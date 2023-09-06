2e60f4a53cb7b8be4f10a10cc3bddef957bffd0a
Asthay97 <asthayadav@asthas-MacBook-Air.local>
2023-09-06 18:13:13 +0800

    ad

--- client/client4.js
+++ client/client4.js
@@ -14,33 +14,60 @@ peer.init()
 
 const orderBook = new OrderBook();
 
+
 // // const trade1 = orderBook.executeTrade();
 // const currentOrderBook = orderBook.getOrderBook();
-
-peer.request('pushOrder', { msg: currentOrderBook }, { timeout: 10000 }, (err, data) => {
-  if (err) {
-    console.error(err)
-    process.exit(-1)
+const f1 = async (orders, amount, quantity) => {
+  if(orders === 'buy'){
+    orderBook.placeBuyOrder(100, 10);
   }
-  // push sell and buy orders to server
-  orderBook.placeBuyOrder(100, 10);
-  orderBook.placeSellOrder(102, 6);
-})
-
-peer.request('updateOrderBook', { msg: currentOrderBook }, { timeout: 10000 }, (err, data) => {
-  if (err) {
-    console.error(err)
-    process.exit(-1)
+  else{
+    orderBook.placeSellOrder(102, 6);
   }
-  // listen to server to update own order book
-  orderBook = data;
-  console.log(JSON.stringify(data))
-})
+  const currentOrderBook = orderBook.getOrderBook();
 
-peer.request('matchOrder', { msg: currentOrderBook }, { timeout: 10000 }, (err, data) => {
-  if (err) {
-    console.error(err)
-    process.exit(-1)
+  peer.request('pushOrder', { msg: currentOrderBook }, { timeout: 10000 }, (err, data) => {
+    if (err) {
+      console.error(err)
+      process.exit(-1)
+    }
+    console.log('current order book', currentOrderBook);
+  })
+}
+
+setInterval(function () {
+  peer.request('updateOrderBook', { msg: currentOrderBook }, { timeout: 10000 }, (err, data) => {
+    if (err) {
+      console.error(err)
+      process.exit(-1)
+    }
+    // listen to server to update own order book
+    orderBook = data;
+    console.log(JSON.stringify(data))
+  })
+}, 1000)
+
+setInterval(function () {
+  if (this.buyOrders.length === 0 || this.sellOrders.length === 0) {
+    return "No orders to trade"; // No orders to trade
   }
-  // check if order matches, send to server to remove the order and re-add the order
-})
+
+  const bestBuyOrder = this.buyOrders[0];
+  const bestSellOrder = this.sellOrders[0];
+  
+  // check if order matches
+  if (bestBuyOrder.price >= bestSellOrder.price) {
+    // send to server to remove the order
+
+    peer.request('matchOrder', { msg: currentOrderBook }, { timeout: 10000 }, (err, data) => {
+      if (err) {
+        console.error(err)
+        process.exit(-1)
+      }
+      // re-add the order
+    })
+    executeTrade()
+    //remaining add
+
+  }
+}, 1000)
